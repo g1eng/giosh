a74825f57a4f821d4205d11d0cac5d15f16408ee
suzume <suzume@localhost>
2021-05-09 04:57:49 +0900

    * replaced pipeline logic with recursive io.Copy between Stdin|StdoutPipe (s) and os.Stdin|Stdout. * suppressed debug messages (now still buggy. The command needs two enter key to gain any output )

--- shell/commandLine.go
+++ shell/commandLine.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	gioParser "github.com/g1eng/giop/core"
 	"io"
-	"log"
+	//"log"
 	"os"
 	"os/exec"
 	"regexp"
@@ -37,9 +37,9 @@ func GetPsString() string {
 //It returns processed expression
 func trimExpressionHead(expr []string) []string {
 	if expr[0] == "" {
-		for i := range expr[0] {
-			log.Printf("expr[%d]: %x", i, []byte(expr[i]))
-		}
+		//for i := range expr[0] {
+		//	log.Printf("expr[%d]: %x", i, []byte(expr[i]))
+		//}
 		expr = expr[1:]
 		expr = trimExpression(expr)
 	}
@@ -88,7 +88,11 @@ func (c *CommandLine) setExpression(lex string) {
 
 // registerCommand set exec.Command object for shell.CommandLine struct.
 func (c *CommandLine) registerCommand(cmdName string, args []string) {
-	var cmd *exec.Cmd
+	var (
+		tmpIndex int
+		cmd      *exec.Cmd
+	)
+	tmpIndex = len(c.cmd)
 	if args == nil || args[0] == "" {
 		cmd = exec.Command(cmdName)
 		c.cmd = append(c.cmd, cmd)
@@ -97,7 +101,14 @@ func (c *CommandLine) registerCommand(cmdName string, args []string) {
 		c.cmd = append(c.cmd, cmd)
 	}
 	lineNo++
-	c.pipe = append(c.pipe, PipeIO{})
+	stdin, err := c.cmd[tmpIndex].StdinPipe()
+	c.track(err)
+	stdout, err := c.cmd[tmpIndex].StdoutPipe()
+	c.track(err)
+	c.pipe = append(c.pipe, PipeIO{
+		stdin:  stdin,
+		stdout: stdout,
+	})
 }
 
 // getCurrentCommand returns latest exec.Command registered in CommandLine.cmd
@@ -113,7 +124,7 @@ func (c *CommandLine) getCurrentCommand() *exec.Cmd {
 // isPipeEnd detects whether the pipe is end or not and returns bool value
 func (c *CommandLine) isPipeEnd() bool {
 	if c.tmpIndex == len(c.lexicalScope)-1 {
-		log.Println("pipe end")
+		//log.Println("pipe end")
 	}
 	return c.tmpIndex == len(c.lexicalScope)-1
 }
@@ -122,10 +133,10 @@ func (c *CommandLine) isPipeEnd() bool {
 // and returns bool value
 func (c *CommandLine) isBlankLine() bool {
 	if len(c.lexicalScope) == 0 {
-		log.Println("blank line")
+		//log.Println("blank line")
 		return true
 	} else if len(c.expression) != 0 && len(c.expression[0]) == 0 {
-		log.Println("blank line")
+		//log.Println("blank line")
 		return true
 	}
 	return false
@@ -134,79 +145,70 @@ func (c *CommandLine) isBlankLine() bool {
 // Exec is a ParserFunc, which returns the result string of the command execution
 func (c *CommandLine) Exec(_ *gioParser.GioParser, s string) (string, error) {
 	var (
-		cmdName      string
-		args         []string
-		originOutput []byte
-		err          error
+		cmdName  string
+		args     []string
+		copySrc  io.Reader
+		copyDest io.Writer
 	)
 	c.Flush()
 	c.lexicalScope = strings.Split(s, "|")
 
 	for i := range c.lexicalScope {
 		c.tmpIndex = i
-		log.Printf("lexicalScope[%d]: %v", i, c.lexicalScope[i])
+		//log.Printf("lexicalScope[%d]: %v", i, c.lexicalScope[i])
+
+		c.setExpression(c.lexicalScope[i])
+		if i == 0 && c.isBlankLine() {
+			return GetPsString(), nil
+		}
+
+		// command with no arg
+		if len(c.expression[i][0]) == 1 {
+			cmdName = c.expression[i][0]
+		} else { //command with arguments
+			cmdName = c.expression[i][0]
+			args = c.expression[i][1:]
+			args = trimExpression(args)
+		}
+
+		c.registerCommand(cmdName, args)
+
+		// debugger
+		//log.Printf("expression[%d]: %v", i, c.expression[i])
+		//for j := range c.expression[i] {
+		//	log.Printf("expression[%d][%d]: %v", i, j, c.expression[i][j])
+		//}
+		//log.Printf("cmdName %d: %s", i, cmdName)
+		//log.Printf("args %d: %v", i, args)
+
+		c.track(c.cmd[i].Start())
+	}
+
+	for i := range c.lexicalScope {
 		if i == 0 {
-			c.setExpression(c.lexicalScope[i])
-			if c.isBlankLine() {
-				return GetPsString(), nil
-			} else if len(c.expression[i][0]) == 1 {
-				cmdName = c.expression[i][0]
-			} else {
-				cmdName = c.expression[i][0]
-				args = c.expression[i][1:]
-				args = trimExpression(args) //trim line-head space characters
-				for j := range c.expression[i] {
-					log.Printf("expression[%d][%d]: %s", j, j, c.expression[i][j])
-				}
-				for j := range args {
-					log.Printf("args[%d]: %s", j, args[j])
-				}
-			}
-			//register the first command
-			c.registerCommand(cmdName, args)
-
-			if c.isPipeEnd() {
-				originOutput, err = c.getCurrentCommand().Output()
-				c.track(err)
-				c.WriteTo(os.Stdout, originOutput)
-				return GetPsString(), err
-			} else {
-				c.pipe[i].stdout, err = c.getCurrentCommand().StdoutPipe()
-				c.track(err)
-				if err != nil {
-					return GetPsString(), err
-				}
-			}
+			copySrc = os.Stdin
 		} else {
-			//stdout1, err := c.getCurrentCommand().StdoutPipe()
-			c.track(err)
-			c.setExpression(c.lexicalScope[i])
-			tmpIndex := len(c.expression) - 1
-			cmdName = c.expression[tmpIndex][0]
-			args = c.expression[tmpIndex][1:]
-			args = trimExpression(args) //trim line-head space characters
-			log.Printf("expression[%d]: %v", i, c.expression[i])
-			for j := range c.expression[i] {
-				log.Printf("expression[%d][%d]: %v", i, j, c.expression[i][j])
-			}
-
-			c.registerCommand(cmdName, args)
-			cmd2 := c.getCurrentCommand()
-
-			stdin2, _ := cmd2.StdinPipe()
-			c.WriteTo(stdin2, originOutput)
-			err = stdin2.Close()
-
-			processOutput, _ := cmd2.Output()
-			c.WriteTo(os.Stdout, processOutput)
+			copySrc = c.pipe[i-1].stdout
 		}
-	}
-	for i := range c.error {
-		if c.error[i] != nil {
-			return GetPsString(), err
+		if i == len(c.cmd)-1 {
+			copyDest = os.Stdout
+		} else {
+			copyDest = c.pipe[i+1].stdin
 		}
+		_, err := io.Copy(c.pipe[i].stdin, copySrc)
+		c.track(err)
+		c.track(c.pipe[i].stdin.Close())
+		_, err = io.Copy(copyDest, c.pipe[i].stdout)
+		c.track(err)
 	}
-	c.WriteTo(os.Stdout, []byte(GetPsString()))
+
+	for i := range c.cmd {
+		c.track(c.cmd[i].Wait())
+	}
+	defer c.track(c.pipe[0].stdin.Close())
+	_, err := c.pipe[0].stdin.Write([]byte("\n"))
+	c.track(err)
+	c.track(c.pipe[len(c.pipe)-1].stdout.Close())
 
 	return GetPsString(), nil
 }
