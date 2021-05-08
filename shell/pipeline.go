7bfe9e2abf336b63db0d84b9bb29e04995294e50
suzume <suzume@localhost>
2021-05-08 15:07:42 +0900

    * modified poc: specified command output passed to a pipe

--- shell/pipeline.go
+++ shell/pipeline.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"regexp"
 )
 
 type PipeIO struct {
@@ -16,9 +17,23 @@ type PipeIO struct {
 }
 
 //PocPipe is a simple poc for line conversion with tr command.
-//All lowercase characters from stdin are converted to uppercase within a pipeline
+//All lowercase characters for the first command output are converted to uppercase within a pipeline
 func (p *PipeIO) PocPipe(_ *gioParser.GioParser, s string) (string, error) {
-	cmd := exec.Command("printf", s)
+	var (
+		cmdLine []string
+		cmdName string
+		args    []string
+	)
+	cmdLine = regexp.MustCompilePOSIX(" ").Split(s, -1)
+	if len(cmdLine) != 0 {
+		cmdName = cmdLine[0]
+	} else {
+		return GetPsString(), nil
+	}
+	if len(cmdLine) > 1 {
+		args = cmdLine[1:]
+	}
+	cmd := exec.Command(cmdName, args...)
 	cmd2 := exec.Command("tr", "a-z", "A-Z")
 	stdin2, _ := cmd2.StdinPipe()
 	//stdout, _ := cmd2.StdoutPipe()
@@ -31,7 +46,7 @@ func (p *PipeIO) PocPipe(_ *gioParser.GioParser, s string) (string, error) {
 	err = stdin2.Close()
 
 	b2, _ := cmd2.Output()
-	_, err = io.WriteString(os.Stdout, string(b2) + " ")
+	_, err = io.WriteString(os.Stdout, string(b2)+" ")
 	if err != nil {
 		return "", err
 	}
