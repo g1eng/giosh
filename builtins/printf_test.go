25ffa07582181ac3a1139bafcd17b666fd29445f
Suzume Nomura <suzume315@g00.g1e.org>
2021-05-29 19:09:00 +0900

    fixed corrupted import declaration printf_test.go

--- builtins/printf_test.go
+++ builtins/printf_test.go
@@ -1,18 +1,17 @@
 package builtins
 
 import (
-	"github.com/g1eng/giosh/builtins"
 	"testing"
 )
 
 func TestPrintfFailsWithNoArgument(t *testing.T) {
-	if err := builtins.Printf(); err == nil {
+	if err := Printf(); err == nil {
 		t.Fatal("printf must make error on no arguments")
 	}
 }
 
 func TestPrintfFailsWithSingleArgument(t *testing.T) {
-	if err := builtins.Printf("a"); err == nil {
+	if err := Printf("a"); err == nil {
 		t.Fatal("printf must make error on single argument")
 	}
 }
