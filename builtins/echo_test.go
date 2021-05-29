0b841d16652ff94b7746616912a01ab10f4fe3e4
Suzume Nomura <suzume315@g00.g1e.org>
2021-05-29 19:41:26 +0900

    migrated verbose error messages into comment lines

--- builtins/echo_test.go
+++ builtins/echo_test.go
@@ -2,26 +2,22 @@ package builtins
 
 import . "gopkg.in/check.v1"
 
+//echo with no arguments should exit without failure"
 func (s *TestSuite) TestEchoNoArg(c *C) {
-	if !c.Check(echo(), IsNil) {
-		c.Fatal("echo with no arguments should exit without failure")
-	}
+	c.Check(echo(), IsNil)
 }
 
+//echo with single valid string should exit without failure
 func (s *TestSuite) TestEchoSingle(c *C) {
-	if !c.Check(echo("ok"), IsNil) {
-		c.Fatal("echo with single valid string should exit without failure")
-	}
+	c.Check(echo("ok"), IsNil)
 }
 
+//echo with three valid strings should exit without failure
 func (s *TestSuite) TestEchoMulti(c *C) {
-	if !c.Check(echo("this", "is", "ok"), IsNil) {
-		c.Fatal("echo with three valid strings should exit without failure")
-	}
+	c.Check(echo("this", "is", "ok"), IsNil)
 }
 
+//echo without newline should exit without failure
 func (s *TestSuite) TestEchoNoNewline(c *C) {
-	if !c.Check(echo("-n", "this", "is", "ok"), IsNil) {
-		c.Fatal("echo without newline should exit without failure")
-	}
+	c.Check(echo("-n", "this", "is", "ok"), IsNil)
 }
